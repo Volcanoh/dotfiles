54db4b822f086bc10b9f84e4064bea1c1c01d030
Can HUANG <huang.volcano@gmail.com>
2016-04-09 22:21:33 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@
 ln -s ~/dotfiles/vim ~/.vim
 ln -s ~/dotfiles/vim/vimrc ~/.vimrc
 #zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 ln -s ~/dotfiles/zshrc ~/.zshrc
 #tmux 
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
