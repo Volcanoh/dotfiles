91b756013f06260596f5f39f42ec36eee94ea47a
huangcan <huangcan@megvii.com>
2018-10-25 13:25:59 +0800

    update for mac

--- vim/ycm_extra_conf.py
+++ vim/ycm_extra_conf.py
@@ -21,6 +21,20 @@ BASE_FLAGS = [
         '/usr/include/eigen3/',
         '-isystem',
         '/home/volcanoh/catkin_ws/src/msckf_mono/include/',
+        '-isystem',
+        '/Library/Developer/CommandLineTools/usr/include/c++/v1',
+        '-isystem',
+        '/usr/local/include',
+        '-isystem',
+        '/Library/Developer/CommandLineTools/usr/lib/clang/10.0.0/include',
+        '-isystem',
+        '/Library/Developer/CommandLineTools/usr/include',
+        '-isystem',
+        '/usr/include',
+        '-isystem',
+        '/System/Library/Frameworks',
+        '-isystem',
+        '/Library/Frameworks',
         ]
 
 SOURCE_EXTENSIONS = [
